f83fc78be808a9b5c2a270540cee715b080ed2af
coffeecat0518 <10602245@live.asia.edu.tw>
2017-10-16 15:11:52 +0800

    java1016

---
+++ src/RUnicode.java
@@ -0,0 +1,9 @@
+public class RUnicode {
+    public static void main(String[] args){
+        java.util.Scanner sc=new java.util.Scanner(System.in);
+
+        char ch = sc.next().charAt(0);
+        System.out.println(Integer.toHexString(ch));
+
+    }
+    }
